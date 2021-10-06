0d038a16c957c450e417b91391b2665c1f2af282
aishwarya <213050010@iitb.ac.in>
2021-10-06 16:02:37 +0530

    Initial template and db push by Aishwarya Mishra on 6/10/21

---
+++ swLab/jobHunt/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.db.models.deletion import CASCADE
+
+# Create your models here.
+
+class Seeker(models.Model):
+    email=models.CharField(max_length=100)
+    location1=models.CharField(max_length=100)
+    location2=models.CharField(max_length=100)
+    location2=models.CharField(max_length=100)
+
+class Job_preference(models.Model):
+    email=models.ForeignKey(Seeker,on_delete=models.CASCADE)
+    job_title=models.CharField(max_length=30)
+
+class Job(models.Model):
+    job_title=models.CharField(max_length=30)
+    tag=models.CharField(max_length=50)
+    company=models.CharField(max_length=100)
+    location=models.CharField(max_length=100)
+    salary_range=models.CharField(max_length=50)
+    link=models.CharField(max_length=200)
+    experience=models.CharField(max_length=30)
+    content=models.CharField(max_length=200)
+
---
+++ swLab/jobHunt/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    return render(request,"index.html")
+
+def search(request):
+    return render(request,"search.html")
+
+def login(request):
+    return render(request,"login.html")
+
+def register(request):
+    return render(request,"register.html")
+
+def update(request):
+    return render(request,"update.html")
+
