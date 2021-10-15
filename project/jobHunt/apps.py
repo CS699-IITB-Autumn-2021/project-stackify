829215212ca100f12cd0438d820c1f9257ca90ea
codecasualty <trycodeforfun@gmail.comm>
2021-10-15 12:09:53 +0530

    added scheduler code subodh oct15

---
+++ project/jobHunt/apps.py
@@ -0,0 +1,9 @@
+from django.apps import AppConfig
+
+
+class JobhuntConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'jobHunt'
+    def ready(self):
+        from webScrapping import updater
+        updater.start()
