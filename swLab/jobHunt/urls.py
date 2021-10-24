35650959cc71e9a5f44c2cac336d3946a5205fe9
aishwarya <213050010@iitb.ac.in>
2021-10-24 17:46:11 +0530

    periodic scheduling of both the parsers and notifier.

--- swLab/jobHunt/urls.py
+++ swLab/jobHunt/urls.py
@@ -14,6 +14,9 @@ urlpatterns = [
     path('index',views.index,name='index'),
     path('search',views.search,name='search'),
     path('login',views.login,name='login'),
+    path('verify_login',views.verify_login,name='verify_login'),
+    path('verify_register',views.verify_register,name='verify_register'),
+    path('make_update',views.make_update,name='make_update'),
     path('register',views.register,name='register'),
     path('update',views.update,name='update')
 ]
